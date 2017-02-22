237d4f28cf5687100dedab9ce7d7f71944770302
Greg Graham <greg.graham@gmail.com>
2017-02-21 21:35:54 -0500

    Rebuilding site Tue Feb 21 21:35:54 EST 2017

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo -e "\033[0;32mDeploying website source to GitHub...\033[0m"
 
 # Build the project.
-hugo # if using a theme, replace by `hugo -t <yourtheme>`
+hugo -t hyde-y
 
 # Add changes to git.
 git add -A
