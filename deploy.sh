88747c6996f81fdd11f1c9c0c1cbc5835e573785
Greg Graham <greg.graham@gmail.com>
2017-02-21 21:28:09 -0500

    Rebuilding site Tue Feb 21 21:28:09 EST 2017

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mDeploying website source to GitHub...\033[0m"
 
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
@@ -19,4 +19,5 @@ git commit -m "$msg"
 git push origin master
 
 # Push the build repos.
+echo -e "\033[0;32mDeploying website to GitHub...\033[0m"
 cd public && git add --all && git commit -m "$msg" && git push origin master && cd ..
